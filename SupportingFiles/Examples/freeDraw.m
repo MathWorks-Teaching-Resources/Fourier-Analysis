ceadc28e0ce4d9aad2b3913cf8a6e15635f2f20c
bhong <bhong@mathworks.com>
2021-11-16 16:43:46 -0500

    -

--- SupportingFiles/Examples/freeDraw.m
+++ SupportingFiles/Examples/freeDraw.m
@@ -1,22 +1,29 @@
- function [x,y,pHandle] = freeDraw(UIFigure,UIAxesHandle,closed)
+function [x,y,pHandle,markerHandle] = freeDraw(src,event,UIFigure,...
+                                UIAxesHandle,color,closed,xmin,xmax,ymin,ymax)
     % Allows the user to draw freely
     % Wait until this is done
+
+    N = 10;
     
+    % Clear the axes
+    cla(UIAxesHandle)
+
     % Start by removing the original motion/click callbacks(replace
     % them later)
-    set(UIFigure,"Pointer","crosshair")
+    set(UIFigure,"Pointer","hand")
     origMotionFcn = UIFigure.WindowButtonMotionFcn;
     origButtonFcn = UIFigure.WindowButtonDownFcn;
     
-    x = NaN;
-    y = NaN;
-    areaPlot = [];
+    location = checkIfInFieldAxis(event,UIAxesHandle,xmin,xmax,ymin,ymax);
+    x = location(1);
+    y = location(2);
+    drawing = true;
+
     hold(UIAxesHandle,"on")
-        pHandle = plot(UIAxesHandle,x,y,'linewidth',2,"color",[0.8,0.4,0.4]);
+        markerHandle = plot(UIAxesHandle,x,y,'o',"color",color,'markerfacecolor',color);
+        pHandle = plot(UIAxesHandle,x,y,'linewidth',2,"color",color);
     hold(UIAxesHandle,"off")
-    
-    drawing = false;
-    
+
     UIFigure.WindowButtonMotionFcn = @addPoint;
     UIFigure.WindowButtonDownFcn = @clickCallback;
      
@@ -32,27 +39,16 @@
             
             return
         end
-        
-        % Start drawing
-        location = SharedFunctions.checkIfInFieldAxis(event,...
-             UIAxesHandle,xmin,xmax,ymin,ymax);
-        if(numel(location) == 2 && ~any(isnan(location)) )
-            x = location(1);
-            y = location(2);
-            pHandle.XData(1) = x;
-            pHandle.YData(1) = y;
-            drawing = true;
-            set(UIFigure,"Pointer","Cross")
-        end
     end
 
     function addPoint(object,event)
         if(drawing)
-            location = SharedFunctions.checkIfInFieldAxis(event,UIAxesHandle);
-         
+            location = checkIfInFieldAxis(event,UIAxesHandle,xmin,xmax,ymin,ymax);
+  
              if(numel(location) == 2 && ~any(isnan(location)) )
                 x = [x;location(1)];
                 y = [y;location(2)];
+
                 pHandle.XData = x;
                 pHandle.YData = y;
                 
@@ -63,6 +59,25 @@
 
                 pause(0);
              end
+        end
+    end
+
+    function location = checkIfInFieldAxis(event,UIAxesHandle,xmin,xmax,ymin,ymax)
+        % Identify if inside the field and return location (this axis
+        % is inside a panel)
+        cp = event.Source.CurrentPoint;
+        axPos =UIAxesHandle.Position;
+        location = NaN; % Return NaN if outside
+        if( cp(1) > axPos(1) && cp(1) < axPos(1) + axPos(3) &&...
+            cp(2) > axPos(2) && cp(2) < axPos(2) + axPos(4) )
+            % If so, update the current position
+            loc = get(UIAxesHandle,'CurrentPoint');
+            loc = loc(1,1:2);
+            if(loc(1)>xmin && loc(1)<xmax && loc(2)>ymin && loc(2)<ymax)
+                
+                location = loc;
+
+            end               
         end
     end
 end
