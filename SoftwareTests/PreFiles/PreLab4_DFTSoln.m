649bc413fd720a2c76a91aa48e695a142e1cd4bd
Valentin Boutrouche <vboutrou@mathworks.com>
2024-10-17 13:24:32 -0400

    Maintenance cycle R2024b

--- SoftwareTests/PreFiles/PreLab4_DFTSoln.m
+++ SoftwareTests/PreFiles/PreLab4_DFTSoln.m
@@ -1,5 +1,10 @@
-%  Pre-run script for Lab4_DFTSoln.mlx
+%  Pre-run script for ComplexFourierSeries.mlx
 % ---- Known Issues     -----
 KnownIssuesID = "";
 % ---- Pre-run commands -----
- 
+sound = @(x,y) disp("Using soundsc");
+audioread = @(x) NewAudioRead(x);
+function varargout=NewAudioRead(varargin)
+load(fullfile(currentProject().RootFolder,"SoftwareTests","PreFiles","Lab4_DFTSoln.mat"));
+varargout={y2Ch,Fs};
+end
